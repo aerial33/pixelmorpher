b7490271b47fbe5cae6d5c1eb5df4698eec2bb0c
Fouad <fouadezari@gmail.com>
2024-03-05 00:23:15 +0100

    feat: add Clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({
 	subsets: ['latin'],
@@ -20,10 +21,12 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang='en'>
-			<body className={cn('font-inter antialiased', inter.variable)}>
-				{children}
-			</body>
-		</html>
+		<ClerkProvider appearance={{ variables: { colorPrimary: '#624cf5' } }}>
+			<html lang='en'>
+				<body className={cn('font-inter antialiased', inter.variable)}>
+					{children}
+				</body>
+			</html>
+		</ClerkProvider>
 	)
 }
